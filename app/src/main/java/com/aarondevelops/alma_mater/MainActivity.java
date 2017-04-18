04ba5896b68d9adcff245004f96abab7612692f1
Aaron <aaronknobloch92@gmail.com>
2017-04-18 10:02:00 -0400

    Adds about message

--- app/src/main/java/com/aarondevelops/alma_mater/MainActivity.java
+++ app/src/main/java/com/aarondevelops/alma_mater/MainActivity.java
@@ -1,6 +1,8 @@
 package com.aarondevelops.alma_mater;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -29,13 +31,36 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if (id == R.id.action_settings)
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_about)
         {
-            return true;
+            showAboutMessage();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showAboutMessage()
+    {
+        new AlertDialog.Builder(this)
+
+                .setMessage(getString(R.string.about_dialog))
+
+                .setPositiveButton(getString(R.string.about_confirmation),
+                        new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                dialog.cancel();
+                            }
+                        })
+
+                .show();
     }
 }
